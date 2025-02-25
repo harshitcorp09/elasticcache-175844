08ed913f5b72885f2a87b796068c167fee2377ca
harshitcorp09 <162092528+harshitcorp09@users.noreply.github.com>
2025-02-25 10:45:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.86.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "ap-south-1"
+}
+
+resource "aws_elasticache_subnet_group" "example" {
+  name       = "example-subnet-group"
+  subnet_ids = ["subnet-96e89bda", "subnet-b16ce9ca"] # Replace with valid subnet IDs
+}
+
+resource "aws_elasticache_replication_group" "example" {
+  replication_group_id       = "example-redis"
+  description                = "Example Redis replication group"
+  node_type                  = "cache.t3.micro"
+  engine                     = "redis"
+  engine_version             = "6.2"  # Deploy Redis 6.2 first
+  port                       = 6379
+  automatic_failover_enabled = true
+  num_cache_clusters         = 2
+  subnet_group_name          = aws_elasticache_subnet_group.example.name
 }
