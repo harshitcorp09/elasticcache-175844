b32820ecff9231b69c9792c86b84f2321c6cc301
harshitcorp09 <162092528+harshitcorp09@users.noreply.github.com>
2025-02-26 10:53:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,20 +9,4 @@ terraform {
 provider "aws" {
   region = "ap-south-1"
 }
-resource "aws_elasticache_subnet_group" "example" {
-  name       = "example-subnet-group"
-  subnet_ids = ["subnet-96e89bda", "subnet-b16ce9ca"] # Replace with valid subnet IDs
-}
 
-resource "aws_elasticache_replication_group" "example" {
-  replication_group_id       = "example-redis"
-  description                = "Example Redis replication group"
-  node_type                  = "cache.t3.micro"
-  engine                     = "redis"
-  engine_version             = "7.1"
-  apply_immediately          = "true"
-  port                       = 6379
-  automatic_failover_enabled = true
-  num_cache_clusters         = 2
-  subnet_group_name          = aws_elasticache_subnet_group.example.name
-}
